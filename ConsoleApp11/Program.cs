67601ae1fd743ffbe18818f94c2cedd7ce4633ae
mana.tokorocchi8@gmail.com <mana.tokorocchi8@gmail.com>
2026-04-09 01:26:17 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    public abstract class Employee
+    {
+        public Employee(string id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
